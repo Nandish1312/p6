99d03c1704f325787538cc4f8bcf28c003c719f7
Nandish1312 <76993210+Nandish1312@users.noreply.github.com>
2022-04-30 18:31:23 +0930

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -11,4 +11,79 @@ using namespace std;
 
 Individual* execute(Individual* indPtr, Mutator* mPtr, int k)
 
-{}
+{
+
+Individual* result=mPtr->mutate(indPtr,k);
+
+return result;
+
+}
+
+int main(int argc, char const *argv[]) 
+
+{
+    
+
+string binarystr1;
+
+string binarystr2;
+
+int k1;
+
+int k2;
+
+std::cin >> binarystr1;
+
+std::cin >> k1;
+
+std::cin >> binarystr2;
+
+std::cin >> k2;
+
+Individual *individual1 = new Individual(binarystr1);
+
+Mutator *mutator1= new BitFlip();
+
+Individual *individual2=execute(individual1,mutator1,k1);
+
+Individual *individual3 =new Individual(binarystr2);
+
+Mutator *mutator2 = new Rearrange();
+
+Individual *individual4=execute(individual3,mutator2,k2);
+
+string mutated_string=individual2->getString();
+
+int lengthOfMS=individual2->getLength();
+
+for (int i = 0; i < lengthOfMS; i++) 
+
+{
+    
+cout << mutated_string[i];
+
+}
+
+std::cout << " ";
+
+string R = individual4->getString();
+
+int max=individual4->getMaxOnes();
+
+int lengthOfRearrangeList=individual4->getLength();
+
+for (int i = 0; i < lengthOfRearrangeList; i++) 
+
+{
+    
+std::cout << R[i];
+
+}
+
+std::cout << " ";
+
+std::cout << max << '\n';
+
+return 0;
+
+}
